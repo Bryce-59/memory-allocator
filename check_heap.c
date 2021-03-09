635baa5e360ee33bf1abcb4b368653101360cf2f
Bryce Richardson <bryce01@cs.utexas.edu>
2021-03-08 23:14:10 -0600

    edits to check_heap

--- check_heap.c
+++ check_heap.c
@@ -13,33 +13,33 @@ extern memory_block_t *first_head;
 int check_heap() {
     // the following tests might be coalesced later, but will
     // remain like this for now for the sake of readability--
-    bool consistent = false;
+    bool inconsistent = false;
     // 1: ensure that the memory list remains in memory order
     memory_block_t *test_one = first_head;
     while (test_one->next) {
-        consistent = consistent || (test_one - test_one->next <= 0);
+        inconsistent = inconsistent || (test_one - test_one->next >= 0); // TRUE if fail
         test_one = test_one->next;
     }
-    // 2: ensure that the alignment is correct for all blocks
+    // 2: ensure that there is no overlap between blocks of memory
     memory_block_t *test_two = first_head;
     while (test_two->next) {
-        consistent = consistent || (((unsigned long) test_two & 0xFF) == (unsigned long) test_two);
+        int end_bit = (unsigned long) test_two + get_size(test_two);
+        inconsistent = inconsistent || (unsigned long) test_two->next <= end_bit; // TRUE if fail
         test_two = test_two->next;
     }
-    // 3: ensure that there is no overlap between blocks of memory
+    // 3: ensure that the alignment is correct for all blocks (should align to 16 or 0xFF)
     memory_block_t *test_three = first_head;
     while (test_three->next) {
-        int end_bit = (unsigned long) test_three + get_size(test_three);
-        consistent = consistent || (unsigned long) test_three->next > end_bit;
+        inconsistent = inconsistent || (((unsigned long) test_three & ALIGNMENT) != (unsigned long) test_three); // TRUE if fail
         test_three = test_three->next;
     }
     // 4: ensure that there are no free blocks that are adjacent in memory (coalescence check)
     memory_block_t *test_four = first_head;
     while (test_four->next) {
         if (!is_allocated(test_four) && !is_allocated(test_four->next)) {
-            consistent = consistent || test_four + get_size(test_four) != test_four->next;
+            inconsistent = inconsistent || test_four + get_size(test_four) == test_four->next; // TRUE if fail
         }
         test_four = test_four->next;
     }
-    return !consistent;
+    return !inconsistent; //negate "inconsistent" to get "consistent"
 }
